0dcaede6f7726f1201696dbf6e6771da730af878
Eliab <Eliabsiji@gmail.com>
2024-03-06 16:45:46 +0100

    working on sidebar

---
+++ fashionistapp/resources/views/inc/sidebar.blade.php
@@ -0,0 +1,175 @@
+	<!-- partial:partials/_sidebar.html -->
+    <nav class="sidebar">
+        <div class="sidebar-header">
+          <a href="#" class="sidebar-brand">
+            <span>Fashionista</span>
+          </a>
+          <div class="sidebar-toggler not-active">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div class="sidebar-body">
+          <ul class="nav">
+            <li class="nav-item nav-category">Main</li>
+            <li class="nav-item">
+              <a href="/dashboard" class="nav-link">
+                <i class="link-icon" data-feather="box"></i>
+                <span class="link-title">Dashboard</span>
+              </a>
+            </li>
+            <li class="nav-item nav-category">users & privilages</li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#user" role="button" aria-expanded="false" aria-controls="emails">
+                <i class="link-icon" data-feather="mail"></i>
+                <span class="link-title">Users Management</span>
+                <i class="link-arrow" data-feather="chevron-down"></i>
+              </a>
+              <div class="collapse" id="user">
+                <ul class="nav sub-menu">
+                  <li class="nav-item">
+                    <a href="pages/email/inbox.html" class="nav-link">All Users</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="pages/email/read.html" class="nav-link">Roles List</a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="pages/email/read.html" class="nav-link">Permission</a>
+                    </li>
+                  </ul>
+              </div>
+            </li>
+            <li class="nav-item nav-category">Client Operations</li>
+            <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#client" role="button" aria-expanded="false" aria-controls="emails">
+                  <i class="link-icon" data-feather="mail"></i>
+                  <span class="link-title">Client Management</span>
+                  <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="client">
+                  <ul class="nav sub-menu">
+                    <li class="nav-item">
+                      <a href="pages/email/inbox.html" class="nav-link">All Clients</a>
+                    </li>
+
+                    </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#contact" role="button" aria-expanded="false" aria-controls="emails">
+                  <i class="link-icon" data-feather="mail"></i>
+                  <span class="link-title">Contact Management</span>
+                  <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="contact">
+                  <ul class="nav sub-menu">
+
+                    <li class="nav-item">
+                      <a href="pages/email/read.html" class="nav-link">All contacts</a>
+                    </li>
+
+                    </ul>
+                </div>
+              </li>
+
+
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#contact" role="button" aria-expanded="false" aria-controls="emails">
+                  <i class="link-icon" data-feather="mail"></i>
+                  <span class="link-title">Measurement Mgnt</span>
+                  <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="contact">
+                  <ul class="nav sub-menu">
+
+                    <li class="nav-item">
+                      <a href="pages/email/read.html" class="nav-link">All contacts</a>
+                    </li>
+
+                    </ul>
+                </div>
+              </li>
+
+
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#contact" role="button" aria-expanded="false" aria-controls="emails">
+                  <i class="link-icon" data-feather="mail"></i>
+                  <span class="link-title">Project  Management</span>
+                  <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="contact">
+                  <ul class="nav sub-menu">
+
+                    <li class="nav-item">
+                      <a href="pages/email/read.html" class="nav-link">All contacts</a>
+                    </li>
+
+                    </ul>
+                </div>
+              </li>
+
+
+              <li class="nav-item">
+                <a class="nav-link" data-bs-toggle="collapse" href="#contact" role="button" aria-expanded="false" aria-controls="emails">
+                  <i class="link-icon" data-feather="mail"></i>
+                  <span class="link-title">Orders Management</span>
+                  <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse" id="contact">
+                  <ul class="nav sub-menu">
+
+                    <li class="nav-item">
+                      <a href="pages/email/read.html" class="nav-link">All contacts</a>
+                    </li>
+
+                    </ul>
+                </div>
+              </li>
+
+
+              <li class="nav-item nav-category">Finance Operations</li>
+              <li class="nav-item">
+                  <a class="nav-link" data-bs-toggle="collapse" href="#finance" role="button" aria-expanded="false" aria-controls="emails">
+                    <i class="link-icon" data-feather="mail"></i>
+                    <span class="link-title">Finance Management</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                  </a>
+                  <div class="collapse" id="finance">
+                    <ul class="nav sub-menu">
+                      <li class="nav-item">
+                        <a href="pages/email/inbox.html" class="nav-link">New Payment</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="pages/email/inbox.html" class="nav-link">All Payment</a>
+                      </li>
+                      <li class="nav-item">
+                        <a href="pages/email/inbox.html" class="nav-link">Balance Payment</a>
+                      </li>
+                      </ul>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-bs-toggle="collapse" href="#invoice" role="button" aria-expanded="false" aria-controls="emails">
+                    <i class="link-icon" data-feather="mail"></i>
+                    <span class="link-title">Invoice Management</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                  </a>
+                  <div class="collapse" id="invoice">
+                    <ul class="nav sub-menu">
+
+                      <li class="nav-item">
+                        <a href="pages/email/read.html" class="nav-link">Invoice</a>
+                      </li>
+
+                      </ul>
+                  </div>
+                </li>
+
+        </ul>
+
+        </div>
+       </nav>
+
+
+          <!-- partial -->
