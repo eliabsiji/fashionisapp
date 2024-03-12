2c394218e45f181a25452894587fe48e9796dca7
Anderson0147 <138449630+Anderson0147@users.noreply.github.com>
2024-03-12 03:36:38 -0700

    Update allclients.blade.php

--- fashionistapp/resources/views/client/allclients.blade.php
+++ fashionistapp/resources/views/client/allclients.blade.php
@@ -4,7 +4,7 @@
 
 
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    New Client
+    New Client  
   </button>
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
